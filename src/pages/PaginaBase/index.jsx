a7fe29d2f72401f7fc4f25ae63f3b640cc33bdd6
Alisson Rego <alissondroid07@gmail.com>
2024-06-27 14:30:29 -0300

    criação de rotas e normalização de css com styled components

---
+++ src/pages/PaginaBase/index.jsx
@@ -0,0 +1,10 @@
+import EstilosGlobais from "../../components/EstilosGlobais";
+
+function PaginaBase() {
+  return <main>
+    <EstilosGlobais />
+    <h1>oi</h1>
+  </main>;
+}
+
+export default PaginaBase;
